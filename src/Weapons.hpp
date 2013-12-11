739b1d2f4604da9cba14d14ac6f5fe7021e3f0aa
Mixbo <mixbo@wakowakowako.com>
2013-12-10 21:56:45 -0500

    Conforme a la coding convention

--- src/Weapons.hpp
+++ src/Weapons.hpp
@@ -1,12 +1,13 @@
 class Weapon
 {
-private:
-	std::string name;
-	float fireRate, damage, accuracy, range;
-	int ammo;
-public:
-	Weapon(std::string name);
-	int getRemainingAmmo();
-	bool canShoot();
-	void shoot();
+	public:
+		Weapon(std::string name);
+		int getRemainingAmmo();
+		bool canShoot();
+		void shoot();
+		
+	private:
+		std::string name;
+		float fireRate, damage, accuracy, range;
+		int ammo;
 };
