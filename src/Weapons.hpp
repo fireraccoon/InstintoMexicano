4651623d5880a643659e6effdfdcd9520c028f39
Mixbo <mixbo@wakowakowako.com>
2013-12-10 20:43:14 -0500

    Ajout de fonctions basiques à la classe Weapon

--- src/Weapons.hpp
+++ src/Weapons.hpp
@@ -3,6 +3,10 @@ class Weapon
 private:
 	std::string name;
 	float fireRate, damage, accuracy, range;
+	int ammo;
 public:
 	Weapon(std::string name);
+	int getRemainingAmmo();
+	bool canShoot();
+	void shoot();
 };
