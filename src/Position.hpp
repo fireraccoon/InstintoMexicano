150555cdebfdd2f7ea781857d6ce8bf9d68dfe67
cesarejames <willyjames.music@gmail.com>
2013-12-12 22:01:10 -0500

    Update Position.hpp

--- src/Position.hpp
+++ src/Position.hpp
@@ -4,19 +4,19 @@
 //  Copyright (C) 2013 Team G.O.A.T
 //  This program is free software; you can redistribute it and/or
 //  modify it under the terms of the GNU General Public License
-//	as published by the Free Software Foundation; either version 2
-//	of the License, or (at your option) any later version.
+//        as published by the Free Software Foundation; either version 2
+//        of the License, or (at your option) any later version.
 //
-//	This program is distributed in the hope that it will be useful,
-//	but WITHOUT ANY WARRANTY; without even the implied warranty of
-//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//	GNU General Public License for more details.
+//        This program is distributed in the hope that it will be useful,
+//        but WITHOUT ANY WARRANTY; without even the implied warranty of
+//        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//        GNU General Public License for more details.
 //
-//	You should have received a copy of the GNU General Public License
-//	along with this program; if not, write to the Free Software
-//	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+//        You should have received a copy of the GNU General Public License
+//        along with this program; if not, write to the Free Software
+//        Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 //  
-//  Nom:    	 Position
+//  Nom:             Position
 //  Auteur:     Cesare James
 //  Description: A class managing a position
 //
@@ -27,28 +27,45 @@ class Position
 {
 public:
 
-	Position(float x, float y);
-	~Position();
+        Position(float x, float y);
+        ~Position();
 
 
+		/* METHODS */
 
+		bool equals(Position b);
 
+		/*
+		* Add a certain amount to a coordinate point
+		*/
+		void addX(float amount);
+		void addY(float amount);
+		/*
+		* Add a certain amount both to X and Y
+		*/
+		void add(float amount);
 
-	/* GETTERS & SETTERS */
-	float getX();
-	float getY();
-	void  setX();
-	void  setY();
-	void  set(float x, float y);
+		/*
+		* Substract a certain amount to a coordinate point
+		*/
+		void substractX(float amount);
+		void substractY(float amount);
+
+		/*
+		* substract a certain amount both to X and Y
+		*/
+		void substract(float amount);
+
+        /* GETTERS & SETTERS */
+        float getX();
+        float getY();
+        void  setX(float x);
+        void  setY(float y);
+        void  set(float x, float y);
 
 
 private:
-	float m_x;
-	float m_y;
+        float m_x;
+        float m_y;
 
 };
-
-/* OPERATOR OVERLOADING */
-Position operator+ (const Position& a, const Position& b);
-Position operator- (const Position& a, const Position& b);
-Position operator== (const Position& a, const Position& b);
