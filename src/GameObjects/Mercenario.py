a50b5e4e7751c31a633e790a39b2d267dfc7eb65
fireraccoon <willyjames.music@gmail.com>
2014-12-19 01:39:37 -0500

    Ditch Entity Component

--- src/GameObjects/Mercenario.py
+++
@@ -1,6 +0,0 @@
-from GoatEngine.GameObject import GameObject
-
-
-class Mercenario(GameObject):
-    def __init__(self):
-        super(Mercenario, self).__init__()
--- src/Scenes/PlayGroundScene.py
+++ src/Scenes/PlayGroundScene.py
@@ -1,10 +1,6 @@
 import pygame
 
-from Components.GraphicsComponents import MercenarioGraphicsComponent, GraphicsComponent
-from Components.InputComponents import PlayerInputComponent
-from Components.Components import VelocityComponent, PhysicsComponent, PositionComponent
 from GoatEngine.Engine import GameState
-from GoatEngine.GameObject import GameObject
 
 
 class PlayGroundScene(GameState):
@@ -13,39 +9,18 @@ class PlayGroundScene(GameState):
         self.gameObjects = []  # A list of gameObjects
 
     def init(self, engine):
-        hero = GameObject()
-        hero.addComponent(PlayerInputComponent.ID, PlayerInputComponent())
-        hero.addComponent(PhysicsComponent.ID, PhysicsComponent())
-        hero.addComponent(VelocityComponent.ID, VelocityComponent())
-        hero.addComponent(GraphicsComponent.ID, MercenarioGraphicsComponent())
-        self.gameObjects.append(hero)
-
-
-        babu = GameObject()
-        pos = babu.components[PositionComponent.ID]
-        pos.x = 50
-        pos.y = 50
-        babu.addComponent(PlayerInputComponent.ID, PlayerInputComponent())
-        babu.addComponent(PhysicsComponent.ID, PhysicsComponent())
-        babu.addComponent(VelocityComponent.ID, VelocityComponent())
-        babu.addComponent(GraphicsComponent.ID, MercenarioGraphicsComponent())
-        self.gameObjects.append(babu)
-
-
-
-
+        pass
 
     def handleEvents(self, engine, events):
         # Quit using (x) button window
         if pygame.QUIT in [event.type for event in events]:
             engine.exit()
-        self.events = events
 
     def update(self, engine, delta):
-        [g.update(self, self.events) for g in self.gameObjects]
+        pass
 
     def draw(self, engine):
-        [g.render(engine.screen) for g in self.gameObjects]
+        pass
 
 
 
